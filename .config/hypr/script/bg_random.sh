90c44beb06cd87a93aed3d5a8597136d82c15725
cll <celalcelik.belgeler@gmail.com>
2025-09-07 19:19:40 +0300

    Update bg_random.sh

--- .config/hypr/script/bg_random.sh
+++ .config/hypr/script/bg_random.sh
@@ -5,4 +5,4 @@ IMAGE_FOLDER="$HOME/wallpapers/"
 random_image=$(find "$IMAGE_FOLDER" -type f | shuf -n 1)
 
 swww img "$random_image"
-#swaybg -i "$random_image"
+cp "$random_image" "$IMAGE_FOLDER/wall"
