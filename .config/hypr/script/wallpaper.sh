be81a5b13abad2a107586bd1868f3875a9f294c4
cll <celalcelik.belgeler@gmail.com>
2025-09-09 01:41:09 +0300

    Add files via upload

--- .config/hypr/script/wallpaper.sh
+++ .config/hypr/script/wallpaper.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 WALLPAPER_DIR="$HOME/wallpapers"
+MATUGEN_CONFIG="$HOME/.config/matugen/config.toml"
 
+# Wallpaper listesi
 dmenu() {
   if [ ! -d "$WALLPAPER_DIR" ]; then
     echo "Hata: $WALLPAPER_DIR dizini bulunamadı!"
@@ -18,8 +20,16 @@ main() {
   fi
 
   selected_wallpaper=$(echo "$choice" | sed 's/^img://')
+
+  # Duvar kağıdını ayarla
   swww img "$selected_wallpaper" --transition-type any --transition-fps 60 --transition-duration .5
- cp "$selected_wallpaper" "$HOME/wallpapers/wall"
+  cp "$selected_wallpaper" "$HOME/wallpapers/wall"
+
+  # Matugen ile renk paletini oluştur
+  matugen image -c "$MATUGEN_CONFIG" "$selected_wallpaper"
+
+  # Waybar ve Hyprland renklerini yeniden yükle (post_hook zaten config.toml’de tanımlı)
+  echo "Yeni renkler uygulandı."
 }
 
 main
