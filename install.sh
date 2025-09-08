6d3eae1dfb6db3f65944ac0f7921f1b561c16efd
cll <celalcelik.belgeler@gmail.com>
2025-09-08 12:12:59 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -63,12 +63,26 @@ install_packages() {
     success "Tüm paketler zaten kurulu."
   fi
 
-  # Manuel paketler
-  if [[ -d "$HOME/hypr/source" ]]; then
-    for pkgfile in "$HOME/hypr/source/"*.pkg.tar.zst; do
-      [[ -f "$pkgfile" ]] && sudo pacman -U --needed --noconfirm "$pkgfile"
-    done
-  fi
+ # Manuel paketler
+if [[ -d "$HOME/hypr/source" ]]; then
+  shopt -s nullglob
+  for pkgfile in "$HOME/hypr/source/"*.pkg.tar.zst "$HOME/hypr/source/"*.pkg.tar.gz; do
+    [[ -f "$pkgfile" ]] && sudo pacman -U --needed --noconfirm "$pkgfile"
+  done
+
+  # Eğer sadece kaynak kod arşivi varsa, makepkg ile derle
+  for srcfile in "$HOME/hypr/source/"*.tar.gz; do
+    if [[ -f "$srcfile" && "$srcfile" != *.pkg.tar.gz ]]; then
+      tmpdir=$(mktemp -d)
+      tar -xvf "$srcfile" -C "$tmpdir"
+      pushd "$tmpdir"/* >/dev/null
+      makepkg -si --noconfirm --needed
+      popd >/dev/null
+      rm -rf "$tmpdir"
+    fi
+  done
+  shopt -u nullglob
+fi
 }
 
 #---------------------------#
