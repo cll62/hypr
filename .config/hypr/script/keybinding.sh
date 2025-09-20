958fe3879c95a7c1c009eac15359f82ffcc5bc1b
cll <celalcelik.belgeler@gmail.com>
2025-09-20 19:06:42 +0300

    Update keybinding.sh

--- .config/hypr/script/keybinding.sh
+++ .config/hypr/script/keybinding.sh
@@ -5,10 +5,25 @@ dynamic_bindings() {
     jq -r '.[] | {modmask, key, keycode, description, dispatcher, arg} | "\(.modmask),\(.key)@\(.keycode),\(.description),\(.dispatcher),\(.arg)"' |
     sed -r \
       -e 's/null//' \
+      -e 's,~/.local/share/omarchy/bin/,,' \
       -e 's,uwsm app -- ,,' \
       -e 's/@0//' \
       -e 's/,@/,code:/' \
+      -e 's/,code:10,/,1,/' \
+      -e 's/,code:11,/,2,/' \
+      -e 's/,code:12,/,3,/' \
+      -e 's/,code:13,/,4,/' \
+      -e 's/,code:14,/,5,/' \
+      -e 's/,code:15,/,6,/' \
+      -e 's/,code:16,/,7,/' \
+      -e 's/,code:17,/,8,/' \
+      -e 's/,code:18,/,9,/' \
+      -e 's/,code:19,/,0,/' \
+      -e 's/,code:20,/,-,/' \
+      -e 's/,code:21,/,=,/' \
       -e 's/^0,/,/' \
+      -e 's/,mouse:272,/,MOUSE LEFT,/' \
+      -e 's/,mouse:273,/,MOUSE RIGHT,/' \
       -e 's/^1,/SHIFT,/' \
       -e 's/^4,/CTRL,/' \
       -e 's/^5,/SHIFT CTRL,/' \
@@ -20,46 +35,40 @@ dynamic_bindings() {
       -e 's/^65,/SUPER SHIFT,/' \
       -e 's/^68,/SUPER CTRL,/' \
       -e 's/^69,/SUPER SHIFT CTRL,/' \
-      -e 's/^72,/SUPER ALT,/'
-}
+      -e 's/^72,/SUPER ALT,/' \
+      -e 's/^73,/SUPER SHIFT ALT,/' \
+      -e 's/^76,/SUPER CTRL ALT,/' \
+      -e 's/^77,/SUPER SHIFT CTRL ALT,/'
+}   # ← eksik olan bu kapanıştı
 
 parse_bindings() {
   awk -F, '
-{
-    # Combine the modifier and key (first two fields)
+  {
     key_combo = $1 " + " $2;
-
-    # Clean up: strip leading "+" if present, trim spaces
     gsub(/^[ \t]*\+?[ \t]*/, "", key_combo);
     gsub(/[ \t]+$/, "", key_combo);
 
-    # Use description, if set
     action = $3;
 
     if (action == "") {
-        # Reconstruct the command from the remaining fields
-        for (i = 4; i <= NF; i++) {
-            action = action $i (i < NF ? "," : "");
-        }
+      for (i = 4; i <= NF; i++) {
+        action = action $i (i < NF ? "," : "");
+      }
+      sub(/,$/, "", action);
+      gsub(/(^|,)[[:space:]]*exec[[:space:]]*,?/, "", action);
+      gsub(/^[ \t]+|[ \t]+$/, "", action);
+      gsub(/[ \t]+/, " ", key_combo);
 
-        # Clean up trailing commas, remove leading "exec, ", and trim
-        sub(/,$/, "", action);
-        gsub(/(^|,)[[:space:]]*exec[[:space:]]*,?/, "", action);
-        gsub(/^[ \t]+|[ \t]+$/, "", action);
-        gsub(/[ \t]+/, " ", key_combo);  # Collapse multiple spaces to one
-
-        # Escape XML entities
-        gsub(/&/, "\\&amp;", action);
-        gsub(/</, "\\&lt;", action);
-        gsub(/>/, "\\&gt;", action);
-        gsub(/"/, "\\&quot;", action);
-        gsub(/'"'"'/, "\\&apos;", action);
+      gsub(/&/, "\\&amp;", action);
+      gsub(/</, "\\&lt;", action);
+      gsub(/>/, "\\&gt;", action);
+      gsub(/"/, "\\&quot;", action);
+      gsub(/'"'"'/, "\\&apos;", action);
     }
-
     if (action != "") {
-        printf "%-35s → %s\n", key_combo, action;
+      printf "%-35s → %s\n", key_combo, action;
     }
-}'
+  }'
 }
 
 dynamic_bindings |
